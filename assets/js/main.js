a459a73da4ecacd2b6d5ec45c67e8db7ab47e077
Thiago Barros <thiagob068@gmail.com>
2022-11-27 14:52:26 -0300

    Melhoria aplicadas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
-const pokemonList = document.getElementById('pokemonList')
-const loadMoreButton = document.getElementById('loadMoreButton')
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
+const imgThemeDefinition = document.querySelector("img.img-template");
+const titleTheme = document.querySelector("h1.title-template");
 
-const maxRecords = 151
-const limit = 10
+const maxRecords = 151;
+const limit = 10;
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
@@ -20,28 +22,48 @@ function convertPokemonToLi(pokemon) {
                      alt="${pokemon.name}">
             </div>
         </li>
-    `
+    `;
 }
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
-    })
+        const newHtml = pokemons.map(convertPokemonToLi).join('');
+        pokemonList.innerHTML += newHtml;
+    });
 }
 
-loadPokemonItens(offset, limit)
+function alterThemeDefinition() {
+    const theme = document.querySelector("img[data-theme]");
+    const content = document.querySelector(".content");
+
+    if (theme.getAttribute("data-theme") === "dia") {
+        imgThemeDefinition.src = "/assets/img/noite.png";
+        theme.setAttribute("data-theme", "noite");
+        content.setAttribute("style", "background-color: #111");
+        titleTheme.setAttribute("style", "color: #FFF");
+    }
+    else {
+        imgThemeDefinition.src = "/assets/img/dia.png";
+        theme.setAttribute("data-theme", "dia");
+        content.setAttribute("style", "background-color: #FFF");
+        titleTheme.setAttribute("style", "color: #111");
+    }
+}
+
+loadPokemonItens(offset, limit);
 
 loadMoreButton.addEventListener('click', () => {
-    offset += limit
-    const qtdRecordsWithNexPage = offset + limit
+    offset += limit;
+    const qtdRecordsWithNexPage = offset + limit;
 
     if (qtdRecordsWithNexPage >= maxRecords) {
         const newLimit = maxRecords - offset
-        loadPokemonItens(offset, newLimit)
+        loadPokemonItens(offset, newLimit);
 
         loadMoreButton.parentElement.removeChild(loadMoreButton)
     } else {
-        loadPokemonItens(offset, limit)
+        loadPokemonItens(offset, limit);
     }
-})
+});
+
+imgThemeDefinition.addEventListener("click", alterThemeDefinition);
